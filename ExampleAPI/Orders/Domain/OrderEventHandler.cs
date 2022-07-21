d9d02a8f42da0c74a0606ebd992ed4901ea18c6b
ZachLondono <zachary.londono5@gmail.com>
2022-07-21 13:27:56 -0400

    Added a NotificationHandler to hande all order events.

---
+++ ExampleAPI/Orders/Domain/OrderEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using static ExampleAPI.Orders.Domain.Events;
+
+namespace ExampleAPI.Orders.Domain;
+
+public class OrderEventHandler<TNotification> : INotificationHandler<TNotification> where TNotification : OrderEvent {
+
+    private readonly ILogger<OrderEventHandler<TNotification>> _logger;
+
+    public OrderEventHandler(ILogger<OrderEventHandler<TNotification>> logger) {
+        _logger = logger;
+    }
+
+    public Task Handle(TNotification notification, CancellationToken cancellationToken) {
+        _logger.LogInformation("Storing order event {EventName}", notification.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
