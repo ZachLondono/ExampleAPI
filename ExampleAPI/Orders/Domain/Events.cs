58ef3b1545b6f546e78a6aa2462715829bef0124
ZachLondono <zachary.londono5@gmail.com>
2022-07-21 13:27:07 -0400

    Made all order events inherit from a base class OrderEvent

--- ExampleAPI/Orders/Domain/Events.cs
+++ ExampleAPI/Orders/Domain/Events.cs
@@ -4,12 +4,14 @@ namespace ExampleAPI.Orders.Domain;
 
 public static class Events {
 
-    public record ItemAddedEvent(OrderedItem Item, string Name, int Qty) : DomainEvent;
+    public record OrderEvent() : DomainEvent;
 
-    public record ItemRemovedEvent(OrderedItem Item) : DomainEvent;
+    public record ItemAddedEvent(OrderedItem Item, string Name, int Qty) : OrderEvent;
 
-    public record ItemQtyAdjustedEvent(OrderedItem Item, int Qty) : DomainEvent;
+    public record ItemRemovedEvent(OrderedItem Item) : OrderEvent;
 
-    public record OrderNameChangedEvent(string Name) : DomainEvent;
+    public record ItemQtyAdjustedEvent(OrderedItem Item, int Qty) : OrderEvent;
+
+    public record OrderNameChangedEvent(string Name) : OrderEvent;
 
 }
