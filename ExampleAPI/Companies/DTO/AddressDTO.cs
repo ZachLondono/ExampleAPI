33840be52a042f558fda6c40ebf207cc7a60a629
ZachLondono <zachary.londono5@gmail.com>
2022-07-23 13:49:39 -0400

    override the equals method of AddressDTO to return true if all fields are the same.

--- ExampleAPI/Companies/DTO/AddressDTO.cs
+++ ExampleAPI/Companies/DTO/AddressDTO.cs
@@ -12,4 +12,22 @@ public class AddressDTO {
 
     public string Zip { get; set; } = string.Empty;
 
+    public override bool Equals(object? address) {
+        
+        if(address == null) return false;
+        if (address is not AddressDTO) return false;
+
+        AddressDTO dto = (AddressDTO)address;
+
+        return dto.Line1.Equals(Line1)
+            && dto.Line2.Equals(Line2)
+            && dto.City.Equals(City)
+            && dto.State.Equals(State)
+            && dto.Zip.Equals(Zip);
+
+    }
+
+    public override int GetHashCode() {
+        return HashCode.Combine(Line1, Line2, City, State, Zip);
+    }
 }
