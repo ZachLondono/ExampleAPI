983ed08b5b479d80f4c973581c250e1a8cbd5f7b
ZachLondono <zachary.londono5@gmail.com>
2022-07-23 18:22:31 -0400

    Updated schema -Added primary key constraints to tables. -Added foreign key contraint between ordered items and orders

--- ExampleAPI/SQL/Schema.sql
+++ ExampleAPI/SQL/Schema.sql
@@ -5,7 +5,8 @@
     line2 character varying(255) DEFAULT ''::character varying NOT NULL,
     city character varying(255) DEFAULT ''::character varying NOT NULL,
     state character varying(255) DEFAULT ''::character varying NOT NULL,
-    zip character varying(255) DEFAULT ''::character varying NOT NULL
+    zip character varying(255) DEFAULT ''::character varying NOT NULL,
+    CONSTRAINT companies_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE public.events (
@@ -15,17 +16,25 @@ CREATE TABLE public.events (
     correlationid uuid,
     "timestamp" timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     type character varying(255) NOT NULL,
-    data json NOT NULL
+    data json NOT NULL,
+    CONSTRAINT events_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE public.orders (
+    id uuid NOT NULL,
+    name character varying(255) NOT NULL,
+    CONSTRAINT orders_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE public.ordereditems (
     id uuid NOT NULL,
     orderid uuid NOT NULL,
     name character varying(255) NOT NULL,
-    qty integer NOT NULL
-);
-
-CREATE TABLE public.orders (
-    id uuid NOT NULL,
-    name character varying(255) NOT NULL
+    qty integer NOT NULL,
+    CONSTRAINT ordereditems_pkey PRIMARY KEY (id),
+    CONSTRAINT order_ordereditems FOREIGN KEY (orderid)
+        REFERENCES public.orders (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+        NOT VALID
 );
