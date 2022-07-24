21d6bed26f90771245bfb637d447f9b071452e31
ZachLondono <zachary.londono5@gmail.com>
2022-07-24 13:26:29 -0400

    Added serilog w/ seq for logging and monitoring

--- ExampleAPI/Program.cs
+++ ExampleAPI/Program.cs
@@ -5,11 +5,11 @@ using ExampleAPI.Orders.Data;
 using ExampleAPI.Orders.Domain;
 using MediatR;
 using Microsoft.AspNetCore.HttpOverrides;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,24 +20,48 @@ builder.Services.AddTransient<IRepository<Company>, CompanyRepository>();
 builder.Services.AddTransient<NpgsqlOrderConnectionFactory>();
 builder.Services.AddTransient<INotificationHandler<DomainEvent>, DomainEventHandler<DomainEvent>>();
 
-var app = builder.Build();
+Log.Logger = new LoggerConfiguration()
+                .ReadFrom
+                .Configuration(builder.Configuration)
+                .CreateLogger();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+builder.Host.UseSerilog();
+
+try {
+
+    Log.Information("Application Starting Up");
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment()) {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    //app.UseHttpsRedirection();
+
+    app.UseForwardedHeaders(new ForwardedHeadersOptions {
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+    });
+
+    app.UseSerilogRequestLogging();
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run("http://*:5000");
+
+} catch (Exception ex) {
+
+    Log.Fatal("Application Failed to Start {Exception}", ex);
+
+} finally {
+
+    Log.Information("Shut Down Complete");
+    Log.CloseAndFlush();
+
 }
-
-//app.UseHttpsRedirection();
-
-app.UseForwardedHeaders(new ForwardedHeadersOptions {
-    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-});
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run("http://*:5000");
